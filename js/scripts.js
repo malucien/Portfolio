8d8a62f1c13c1cf467ada28ff4f6fac581b5517f
malucien <124107514+malucien@users.noreply.github.com>
2023-03-05 14:28:33 -0600

    First commit

--- js/scripts.js
+++ js/scripts.js
@@ -6,3 +6,14 @@ window.addEventListener("scroll", function () {
   parallax.style.backgroundPositionY = offset * 0.7 + "px";
   // DIV 1 background will move slower than other elements on scroll.
 });
+
+const nav = document.querySelector('.nav')
+window.addEventListener('scroll', fixNav)
+
+function fixNav() {
+    if(window.scrollY > nav.offsetHeight + 150) {
+        nav.classList.add('active')
+    } else {
+        nav.classList.remove('active')
+    }
+}
